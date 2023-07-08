3b4c3464a881fcf82ac39ddb9401d8131a74229b
Yiling-J <njjyl723@gmail.com>
2023-07-08 16:54:36 +0800

    update build

--- internal/nvm/directio/direct_io_simple.go
+++ internal/nvm/directio/direct_io_simple.go
@@ -17,5 +17,5 @@ const (
 
 // OpenFile is a modified version of os.OpenFile which sets O_DIRECT
 func OpenFile(name string, flag int, perm os.FileMode) (file *os.File, err error) {
-	return os.OpenFile(name, syscall.O_DIRECT|flag, perm)
+	return os.OpenFile(name, flag, perm)
 }
